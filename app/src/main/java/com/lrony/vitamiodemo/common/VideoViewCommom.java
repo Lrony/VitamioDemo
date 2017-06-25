b5902ce12ac2b9d002bef0010b22a958b10619a0
Lrony <5414115@qq.com>
2017-06-25 10:16:56 +0800

    add Video commom class

--- app/src/main/java/com/lrony/vitamiodemo/common/VideoViewCommom.java
+++ app/src/main/java/com/lrony/vitamiodemo/common/VideoViewCommom.java
@@ -1,7 +1,16 @@
 package com.lrony.vitamiodemo.common;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by lrony on 2017/6/25.
  */
-public class VideoViewCommom {
+public class VideoViewCommom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
